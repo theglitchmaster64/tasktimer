359f0a093291467bdf7f153b8707af32bd2a74ee
theglitchmaster64 <theglitchmaster64@gmail.com>
2020-11-16 22:06:38 +0530

    added stats graph

--- tasktimer.py
+++ tasktimer.py
@@ -2,18 +2,27 @@
 
 import os, time, sys
 from threading import Thread
+import matplotlib.pyplot as plotter
+import numpy as np
 
 STOP_THIS = False
+TASK_LOG = {}
+TOTAL_TIME = 0
 
 def timer(countdown):
     global STOP_THIS
+    global TASK_LOG
+    global TOTAL_TIME
     secs = countdown
-    total_time = 0
+    task_no = 0
     while True:
         if STOP_THIS == True:
-            print('task time:  '+str(countdown - secs))
-            total_time += countdown - secs
-            print('total time: '+str(total_time))
+            task_no +=1
+            task_time = countdown - secs
+            TASK_LOG[task_no] = task_time
+            print('task {} time:  '.format(task_no)+str(task_time))
+            TOTAL_TIME += task_time
+            print('total time:   '+str(TOTAL_TIME)+'\n')
             secs = countdown
             STOP_THIS = False
         time.sleep(1)
@@ -24,9 +33,15 @@ def timer(countdown):
 def stop():
     x=input()
     global STOP_THIS
+    global TASK_LOG
     if x == '':
         STOP_THIS = True
     elif x == 'q':
+        fig = plotter.figure(figsize=(16,9))
+        tno = list(TASK_LOG.keys())
+        ttime = list(TASK_LOG.values())
+        plotter.bar(tno,ttime)
+        plotter.savefig('stats.png')
         sys.exit(0)
 
 if __name__=='__main__':
